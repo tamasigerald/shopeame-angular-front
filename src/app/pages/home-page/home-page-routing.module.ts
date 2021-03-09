8df1d4e03922002deada7f849016be1dd58da833
Gerald Andrei Tamasi <tamasigerald@gmail.com>
2021-03-09 21:43:26 +0100

    Fix: missing dep

--- src/app/pages/home-page/home-page-routing.module.ts
+++ src/app/pages/home-page/home-page-routing.module.ts
@@ -1,7 +1,6 @@
 import { HomePageComponent } from './home-page.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { component } from 'vue/types/umd';
 
 const routes: Routes = [
   {path: '', component: HomePageComponent}
